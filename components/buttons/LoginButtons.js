15a5816c3d1d3d8da67521dd75f052abda2c0baf
coolbuhzor <coolbuhzor@gmail.com>
2022-02-07 10:19:44 +0100

    fixed conflicts

--- components/buttons/LoginButtons.js
+++ components/buttons/LoginButtons.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-=======
-import styled from "styled-components";
->>>>>>> 725194780453bfb94cf3a04163eb10feb010a5c7
 import { GoogleIcon } from "../Icons/Icon";
 import { Button } from "./ButtonStyle";
 
