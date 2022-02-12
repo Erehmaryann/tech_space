d77d52f03ca79e3db854015c129b7de60b33631d
erehmaryann <edwardmaryann09@gmail.com>
2022-02-12 12:48:11 -0800

    preloader comp

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+import React from "react";
 import Layout from "../components/layouts/Layout";
+import Preloader from "../components/common/preloader/preloader";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+  }, []);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {!loading ? (
+        <Preloader />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )
+      }
+    </>
   );
 }
 
