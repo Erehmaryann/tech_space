445978c887d82b286afc9cd1e09bf3cc9dfe9e74
erehmaryann <edwardmaryann09@gmail.com>
2022-03-06 21:17:49 -0800

    android/Ios page

---
+++ pages/android-ios.js
@@ -0,0 +1,25 @@
+import Android from '../components/android/android';
+import Head from "next/head";
+import styled from "styled-components";
+//  since you want the login to be the home page.\, we would conditionally render either log in or home page here and then pass it to the index page
+const AndroidIos = () => {
+
+    return (
+        <>
+            <Head>
+                <title>Tech Space | Android/IOS</title>
+            </Head>
+
+            <HomeItemContainer>
+                <Android />
+            </HomeItemContainer>
+        </>
+
+    );
+};
+
+const HomeItemContainer = styled.div`
+  background: #e5e5e5;
+`;
+
+export default AndroidIos;
