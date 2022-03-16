0ed1cad08dab1c0ca5f5a6a2cc69f6688370c1a3
erehmaryann <edwardmaryann09@gmail.com>
2022-03-16 16:43:24 -0700

    install emoji mart

---
+++ components/emoji/emoji.js
@@ -0,0 +1,12 @@
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+
+const Emoji = () => {
+    return (
+        <div>
+            <Picker />
+        </div>
+    );
+};
+
+export default Emoji;
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,8 @@
 import PostsData from "../components/homepage/posts-data";
 import Head from "next/head";
 import styled from "styled-components";
+import Emoji from '../components/emoji/emoji';
+import { Picker } from 'emoji-mart';
 //  since you want the login to be the home page.\, we would conditionally render either log in or home page here and then pass it to the index page
 const home = () => {
   return (
@@ -9,7 +11,8 @@ const home = () => {
         <title>Tech Space | Home</title>
       </Head>
       <HomeItemContainer>
-        <PostsData />
+        {/* <PostsData /> */}
+        <Picker showPreview={false} showSkinTones={false} />
       </HomeItemContainer>
     </>
 
