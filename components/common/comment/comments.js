98196b400bb8fff11097b1be6968fa3758268e10
erehmaryann <edwardmaryann09@gmail.com>
2022-04-24 16:04:25 -0700

    refactored comments comp

--- components/common/comment/comments.js
+++ components/common/comment/comments.js
@@ -3,7 +3,7 @@ import { updateComment as updateCommentApi, getComments as getCommentsApi, creat
 
 import Comment from './comment';
 import CommentForm from "./commentForm";
-import styled from "styled-components";
+import { CommentDiv } from './commentsStyles';
 
 const Comments = ({ currentUserId }) => {
     const [backendComments, setBackendcomments] = useState([]);
@@ -116,38 +116,5 @@ const Comments = ({ currentUserId }) => {
         </CommentDiv>
     );
 };
-
-const CommentDiv = styled.div`
-    background: rgb(255, 255, 255);
-    width: 765px;
-    box-shadow: 0px 4px 4px rgba(55, 73, 86, 0.07);
-    border-radius: 15px;
-    margin-top: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    position: relative;
-
-    .comments-container {
-        margin: 20px 0;
-    }
-
-    .comment_load-more {
-        cursor: pointer;
-        p {
-            position: absolute;
-            right: 18px;
-            font-size: 12px;
-            color: #409DE0;
-            font-weight: 600;
-            bottom: 20px;
-
-            :hover {
-                color: #A2A2A2;
-            }
-        }
-    }
-`;
 
 export default Comments;
---
+++ components/common/comment/commentsStyles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const CommentDiv = styled.div`
+    background: rgb(255, 255, 255);
+    width: 765px;
+    box-shadow: 0px 4px 4px rgba(55, 73, 86, 0.07);
+    border-radius: 15px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    position: relative;
+
+    .comments-container {
+        margin: 20px 0;
+    }
+
+    .comment_load-more {
+        cursor: pointer;
+        p {
+            position: absolute;
+            right: 18px;
+            font-size: 12px;
+            color: #409DE0;
+            font-weight: 600;
+            bottom: 20px;
+
+            :hover {
+                color: #A2A2A2;
+            }
+        }
+    }
+`;
