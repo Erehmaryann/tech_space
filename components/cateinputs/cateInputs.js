f859ffb6cdca1dd1c702d32fee56878e0fe21cdd
Erehmaryann <edwardmaryann09@gmail.com>
2023-01-09 13:26:46 +0100

    refactored width

--- components/cateinputs/cateInputs.js
+++ components/cateinputs/cateInputs.js
@@ -1,146 +1,178 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-key */
-import Select from 'react-select';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { ButDiv, Button, CateDiv } from './cateinputsStyle';
+import Select from "react-select";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { ButDiv, Button, CateDiv } from "./cateinputsStyle";
 
 import Modal from "../modal/Modal";
 
 const options = [
-    { value: 'programming', label: 'Programming' },
-    { value: 'web development', label: 'Web development' },
-    { value: 'networking', label: 'Networking' },
-    { value: 'computer repair and maintenace', label: 'Computer repair and maintenace' },
-    { value: 'android/IOS', label: 'Android/IOS' },
-    { value: 'phone and technology', label: 'Phone and Technology' },
+  { value: "programming", label: "Programming" },
+  { value: "web development", label: "Web development" },
+  { value: "networking", label: "Networking" },
+  {
+    value: "computer repair and maintenace",
+    label: "Computer repair and maintenace",
+  },
+  { value: "android/IOS", label: "Android/IOS" },
+  { value: "phone and technology", label: "Phone and Technology" },
 ];
 
 const SelectStyle = {
-    valueContainer: (provided) => ({
-        ...provided,
-        // height: "33px ",
-        display: "flex",
-        alignItems: "center",
-    }),
-    control: (provided, state) => ({
-        ...provided,
-        width: "100%",
-        marginTop: "5px",
-        height: "100%",
-        background: "#f5f5f5",
-        border: "none",
-        borderRadius: "10px",
-        outline: "none",
-        marginBottom: "10px",
-        boxShadow: state.isFocused ? "none" : 0,
-    }),
-    option: (provided, state) => ({
-        ...provided,
-        display: "flex",
-        justifyContent: "center",
-        textALign: "center",
-        fontStyle: "normal",
-        fontWeight: "300",
-        fontSize: "12px",
-        paddingLeft: "10px",
-        // zIndex: 100000,
-        color: state.isSelected ? "#ffffff" : "rgba(0,0,0,0.9)",
-    }),
+  valueContainer: (provided) => ({
+    ...provided,
+    // height: "33px ",
+    display: "flex",
+    alignItems: "center",
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    width: "100%",
+    marginTop: "5px",
+    height: "100%",
+    background: "#f5f5f5",
+    border: "none",
+    borderRadius: "10px",
+    outline: "none",
+    marginBottom: "10px",
+    boxShadow: state.isFocused ? "none" : 0,
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    display: "flex",
+    justifyContent: "center",
+    textALign: "center",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: "12px",
+    paddingLeft: "10px",
+    // zIndex: 100000,
+    color: state.isSelected ? "#ffffff" : "rgba(0,0,0,0.9)",
+  }),
 
-    singleValue: (provided) => ({
-        ...provided,
-        color: "#9E9E9E",
-        fontStyle: "normal",
-        fontWeight: "300",
-        fontSize: "12px",
-        letterSpacing: "0.06em",
-    }),
-    placeholder: (provided) => ({
-        ...provided,
-        color: "#9E9E9E",
-        fontStyle: "normal",
-        fontWeight: "300",
-        fontSize: "12px",
-        letterSpacing: "0.06em",
-    }),
-    indicatorSeparator: (provided) => ({
-        ...provided,
-        display: "none",
-    }),
+  singleValue: (provided) => ({
+    ...provided,
+    color: "#9E9E9E",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: "12px",
+    letterSpacing: "0.06em",
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: "#9E9E9E",
+    fontStyle: "normal",
+    fontWeight: "300",
+    fontSize: "12px",
+    letterSpacing: "0.06em",
+  }),
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    display: "none",
+  }),
 };
 
 const CateInputs = () => {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [showModal, setShowModal] = useState(false);
-    const [images, setImages] = useState([]);
-    const [imageURLs, setImageURLs] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [images, setImages] = useState([]);
+  const [imageURLs, setImageURLs] = useState([]);
 
-    useEffect(() => {
-        if (images.length < 1) return;
-        const newImageUrls = [];
-        images.forEach(image => {
-            newImageUrls.push(URL.createObjectURL(image));
-        });
-        setImageURLs(newImageUrls);
-    }, [images]);
+  useEffect(() => {
+    if (images.length < 1) return;
+    const newImageUrls = [];
+    images.forEach((image) => {
+      newImageUrls.push(URL.createObjectURL(image));
+    });
+    setImageURLs(newImageUrls);
+  }, [images]);
 
-    const onImageChange = (e) => {
-        setImages([...e.target.files]);
-    };
+  const onImageChange = (e) => {
+    setImages([...e.target.files]);
+  };
 
-    const showModalHandler = () => {
-        setShowModal(true);
-    };
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
 
-    return (
-        <CateDiv>
-            <div className="input-group">
-                <label htmlFor="topic-title">Title</label>
-                <input type="text" name="topic-title" id="topic-title" className="input" placeholder="Topic title" />
-            </div>
-            <div className="input-group">
-                <label htmlFor="category">Add category</label>
-                <Select
-                    defaultValue={selectedOption}
-                    onChange={setSelectedOption}
-                    options={options}
-                    styles={SelectStyle}
-                    id="category"
-                    placeholder="Select a category"
-                    name="category"
-                />
-            </div>
-            <div className="input-group">
-                <label htmlFor="topic-description">Description</label>
-                <textarea name="topic-description" id="topic-description" placeholder="Enter a description" style={{ resize: "none" }} />
-            </div>
-            <div className="input-group">
-                <label htmlFor="file-upload" className="file-upload">
-                    {images.length < 1 ? (
-                        <>
-                            <Image src="/assets/svg/photoIcon.svg" width={20} height={20} alt="Photo-Icon" />
-                            <input type="file" name="file-upload" id="file-upload" accept="image/*" onChange={onImageChange} />
-                        </>
-                    ) :
-                        imageURLs.map(imageSrc => (<Image className="rev" width={40} height={20} src={imageSrc} alt="topic-image" />))
-                    }
-
-                </label>
-            </div>
-            <ButDiv>
-                <Button onClick={() => showModalHandler()}>Create</Button>
-            </ButDiv>
-            <Modal
-                onClose={() => setShowModal(false)}
-                show={showModal}
-                btnText={`ok`}
-                btn
-            >
-                Your topic is pending approval
-            </Modal>
-        </CateDiv>
-    );
+  return (
+    <CateDiv>
+      <div className="input-group">
+        <label htmlFor="topic-title">Title</label>
+        <input
+          type="text"
+          name="topic-title"
+          id="topic-title"
+          className="input"
+          placeholder="Topic title"
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="category">Add category</label>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+          styles={SelectStyle}
+          id="category"
+          placeholder="Select a category"
+          name="category"
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="topic-description">Description</label>
+        <textarea
+          name="topic-description"
+          id="topic-description"
+          placeholder="Enter a description"
+          style={{ resize: "none" }}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="file-upload" className="file-upload">
+          {images.length < 1 ? (
+            <>
+              <Image
+                src="/assets/svg/photoIcon.svg"
+                width={20}
+                height={20}
+                alt="Photo-Icon"
+              />
+              <input
+                type="file"
+                name="file-upload"
+                id="file-upload"
+                accept="image/*"
+                onChange={onImageChange}
+              />
+            </>
+          ) : (
+            imageURLs.map((imageSrc) => (
+              <Image
+                className="rev"
+                width={40}
+                height={20}
+                src={imageSrc}
+                alt="topic-image"
+              />
+            ))
+          )}
+        </label>
+      </div>
+      <ButDiv>
+        <Button onClick={() => showModalHandler()}>Create</Button>
+      </ButDiv>
+      <Modal
+        onClose={() => setShowModal(false)}
+        show={showModal}
+        btnText={`ok`}
+        btn
+      >
+        Your topic is pending approval
+      </Modal>
+    </CateDiv>
+  );
 };
 
 export default CateInputs;
--- components/common/comment/commentStyles.js
+++ components/common/comment/commentStyles.js
@@ -1,76 +1,75 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Div = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+
+  .comment-image-container {
+    margin-right: 12px;
+  }
+
+  .comment-right-part {
+    width: 100%;
+    .grey-bg {
+      background: #f1f1f1;
+      padding: 1rem 0 1rem 1rem;
+      box-sizing: border-box;
+      border-radius: 0px 10px 10px 10px;
+    }
+  }
+
+  .comment-content {
     display: flex;
-    margin-bottom: 10px;
+    color: #c4c4c4;
+    font-size: 9px;
+    align-items: center;
+    padding-bottom: 0.5rem;
 
-    .comment-image-container {
-        margin-right: 12px;
+    .time {
+      &::before {
+        content: "•";
+        padding-left: 1rem;
+        padding-right: 0.2rem;
+        font-weight: bold;
+      }
     }
+  }
 
-    .comment-right-part {
-        width: 100%;
-        .grey-bg {
-            background: #F1F1F1;
-            padding: 1rem 0 1rem 1rem;
-            box-sizing: border-box;
-            border-radius: 0px 10px 10px 10px;
-        }
+  .comment-author {
+    /* margin-right: 8px; */
+    font-size: 12px;
+    font-weight: 600;
+    color: #374956;
+  }
+
+  .comment-text {
+    font-size: 10px;
+    color: #374956;
+  }
+
+  .comment-actions {
+    display: flex;
+    /* font-size: 12px; */
+    cursor: pointer;
+    margin-top: 8px;
+  }
+
+  .comment-action {
+    margin-right: 8px;
+    font-size: 11px;
+    color: #a2a2a2;
+    font-weight: 400;
+    &::before {
+      content: "•";
+      padding-right: 0.2rem;
     }
+  }
 
-    .comment-content {
-        display: flex;
-        color: #C4C4C4;
-        font-size: 9px;
-        align-items: center;
-        padding-bottom: 0.5rem;
+  .comment-action:hover {
+    color: #409de0;
+  }
 
-        .time {
-            &::before {
-                content: "•";
-                padding-left: 1rem;
-                padding-right: 0.2rem;
-                font-weight: bold;
-            }
-        }
-    }
-
-    .comment-author {
-        /* margin-right: 8px; */
-        font-size: 12px;
-        font-weight: 600;
-        color: #374956;
-        
-    }
-
-    .comment-text {
-        font-size: 10px;
-        color: #374956;
-    }
-
-    .comment-actions {
-        display: flex;
-        /* font-size: 12px; */
-        cursor: pointer;
-        margin-top: 8px;
-    }
-
-    .comment-action {
-        margin-right: 8px;
-        font-size: 11px;
-        color: #A2A2A2;
-        font-weight: 400;
-        &::before {
-            content: "•";
-            padding-right: 0.2rem;
-        }
-    }
-
-    .comment-action:hover {
-        color: #409DE0;
-    }
-
-    .replies {
-        margin-top: 20px;
-    }
+  .replies {
+    margin-top: 20px;
+  }
 `;
