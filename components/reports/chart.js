7c62cf71ad77c366290116021b7359d4f85a3367
coolbuhzor <coolbuhzor@gmail.com>
2023-01-17 17:18:21 +0100

    table component fix

--- components/reports/chart.js
+++ components/reports/chart.js
@@ -10,7 +10,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import { ChartContainer } from "./reportStyles";
+import { ChartStyles } from "./reportStyles";
 
 const data = [
   {
@@ -78,7 +78,7 @@ const data = [
 export default class ReportChart extends PureComponent {
   render() {
     return (
-      <ChartContainer>
+      <ChartStyles>
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             width={672}
@@ -99,7 +99,7 @@ export default class ReportChart extends PureComponent {
             <Bar dataKey="pv" fill="#F7F8F8" />
           </BarChart>
         </ResponsiveContainer>
-      </ChartContainer>
+      </ChartStyles>
     );
   }
 }
