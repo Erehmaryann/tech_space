76fe7fd0a73dda83203a3b70997cdd3d92dda15c
Erehmaryann <edwardmaryann09@gmail.com>
2023-02-09 16:45:55 +0100

    fixed bug

--- middleware.js
+++ middleware.js
@@ -5,10 +5,26 @@ export default function middleware(req) {
   let verify = req.cookies.get("loggedin");
   let url = req.url;
   console.log(url);
-  if ((!verify && url.includes("/home")) || url.includes("/requests")) {
-    return (
-      NextResponse.redirect("http://localhost:3000/") ||
-      NextResponse.redirect("tech-space-eight.vercel.app")
-    );
+  if (
+    (!verify && url.includes("/home")) ||
+    url.includes("/requests") ||
+    url.includes("/android-ios") ||
+    url.includes("/computer-repair") ||
+    url.includes("/create-new-password") ||
+    url.includes("/forgot-password") ||
+    url.includes("/members") ||
+    url.includes("/networking") ||
+    url.includes("/phone-technology") ||
+    url.includes("/profile") ||
+    url.includes("/programming") ||
+    url.includes("/reports") ||
+    url.includes("/saved-topics") ||
+    url.includes("/settings") ||
+    url.includes("/topics") ||
+    url.includes("/trending") ||
+    url.includes("/web-development") ||
+    url.includes("/404")
+  ) {
+    return NextResponse.redirect("http://localhost:3000/");
   }
 }
